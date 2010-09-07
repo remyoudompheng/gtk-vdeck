35b5ecf2b48a28b809900defeaf98fdebc9c1468
Rémy Oudompheng <oudomphe@math.unice.fr>
2010-09-07 15:04:35 +0200

    Stub code for graphical interface.

---
+++ src/MainWindow.hpp
@@ -0,0 +1,47 @@
+/*
+ * math-bibmanager
+ * A Bibliography manager for mathematicians
+ *
+ * Copyright (C) 2010 Rémy Oudompheng
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include <gtkmm.h>
+#include <string>
+
+class MainWindow : public Gtk::Window
+{
+public:
+  MainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refBuilder);
+  virtual ~MainWindow();
+
+protected:
+  Glib::RefPtr<Gtk::Builder> uidef;
+
+  // Callbacks
+  typedef void (MainWindow::*callback)();
+  void connect_action(Glib::ustring name, callback f) {
+    Glib::RefPtr<Gtk::Action> a = Glib::RefPtr<Gtk::Action>::cast_dynamic(uidef->get_object(name));
+    a->signal_activate().connect(sigc::mem_fun(*this, f));
+  };
+
+  void _on_quit_activate();
+};
+
+#endif //!MAIN_WINDOW_H
